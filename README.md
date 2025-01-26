7283beb6cfd7a475fe1a6f54581c908d2dfb64f4
ansar-butt <110402422+ansar-butt@users.noreply.github.com>
2025-01-26 14:56:44 +0100

    Commit

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # LeetCode-Challenges
+
 A public repo detailing my approach to various LeetCode challenges.
 
 ## Challenges Completed:
+
 ### Hard
+
 - [Merge k Sorted Lists](https://leetcode.com/problems/merge-k-sorted-lists) -> kSort.py
 - [Longest Valid Parentheses](https://leetcodecom/problems/longest-valid-parentheses/) -> longestParanthesis.py
 
-
 ### Medium
+
 - [Add Two Numbers](https://leetcodecom/problems/add-two-numbers/description/) -> addTwo.py
+- [Longest Palindromic Substring](https://leetcode.com/problems/longest-palindromic-substring/) -> longestPalindrome.py
 
 ### Easy
-- [Two Sum](https://leetcode.com/problems/two-sum/description/) -> twoSum.py
 
+- [Two Sum](https://leetcode.com/problems/two-sum/description/) -> twoSum.py
---
+++ longestPalindrome.py
@@ -0,0 +1,25 @@
+# 5. Longest Palindromic Substring
+"""
+Given a string s, return the longest palindromic substring in s.
+"""
+
+
+# Space Complexity: 99th Percentile
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def isPalindrome(string: str) -> bool:
+            strLength = len(string)
+            for x in range(0, strLength // 2):
+                if string[x] != string[strLength - x - 1]:
+                    return False
+            return True
+
+        substr = ""
+        for i in range(0, len(s)):
+            for j in range(len(s), 0, -1):
+                if len(substr) > j - i:
+                    break
+                if isPalindrome(s[i:j]):
+                    substr = s[i:j]
+
+        return substr
